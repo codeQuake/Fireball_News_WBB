f1cbbc7f59119c97cb06387ce36514d5dd32193c
Jens Krumsieck <info@codequake.de>
2014-07-29 20:15:30 +0200

    fix

--- files/lib/system/event/listener/FinalizeNewsActionListener.class.php
+++ files/lib/system/event/listener/FinalizeNewsActionListener.class.php
@@ -18,39 +18,36 @@ class FinalizeNewsActionListener implements IEventListener{
 		if ($className != 'cms\form\NewsAddForm') return;
 		$board = BoardCache::getInstance()->getBoard(CMS_NEWS_POST_BOARD);
 		if ($board === null || !$board->isBoard()) return;
-		foreach ($eventObj->getObjects() as $news) {
-			$data = array(
-				'boardID' => $board->boardID,
-				'languageID' => $news->languageID,
-				'topic' => $news->subject,
-				'time' => $news->time,
-				'userID' => $news->userID,
-				'username' => $news->username,
-				'hasLabels' => 0
-			);
-			$threadData = array(
-				'board' => $board,
-				'data' => $data,
-				'postData' => array(
-					'message' => '[news='.$news->newsID.'][/news]',
-					'enableBBCodes' => 1,
-					'enableHtml' => 0,
-					'enableSmilies' => $news->enableSmilies,
-					'showSignature' => $news->showSignature
-				),
-				'tags' => array()
-			);
-
-			//enable tags
-			if (MODULE_TAGGING && WBB_THREAD_ENABLE_TAGS) {
-				$tags = TagEngine::getInstance()->getObjectTags('de.voolia.news.entry', $news->newsID);
-				foreach ($tags as $tag) {
-					$threadData['tags'][] = $tag->name;
-				}
+		print_r($eventObj); exit;
+		$data = array(
+			'boardID' => $board->boardID,
+			'languageID' => $news->languageID,
+			'topic' => $news->subject,
+			'time' => $news->time,
+			'userID' => $news->userID,
+			'username' => $news->username,
+			'hasLabels' => 0
+		);
+		$threadData = array(
+			'board' => $board,
+			'data' => $data,
+			'postData' => array(
+				'message' => '[news='.$news->newsID.'][/news]',
+				'enableBBCodes' => 1,
+				'enableHtml' => 0,
+				'enableSmilies' => $news->enableSmilies,
+				'showSignature' => $news->showSignature
+			),
+			'tags' => array()
+		);
+		//enable tags
+		if (MODULE_TAGGING && WBB_THREAD_ENABLE_TAGS) {
+			$tags = TagEngine::getInstance()->getObjectTags('de.codequake.cms.news', $news->newsID);
+			foreach ($tags as $tag) {
+				$threadData['tags'][] = $tag->name;
 			}
-
-			$action = new ThreadAction(array(), 'create', $threadData);
-			$resultValues = $action->executeAction();
 		}
+		$action = new ThreadAction(array(), 'create', $threadData);
+		$resultValues = $action->executeAction();
 	}
 }
